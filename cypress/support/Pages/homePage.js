1f12967a93beb6b503c5298b219eeab6f71d6e30
marvelousstorm <brian_vinn@hotmail.com>
2025-05-09 20:52:07 -0600

    Making tests

--- cypress/support/Pages/homePage.js
+++ cypress/support/Pages/homePage.js
@@ -11,5 +11,11 @@ class HomePage{
     getProductsortContainer() {
         return cy.get('.product_sort_container',{timeout:15000})
     }
+    getProductNames() {
+        return cy.get('div[data-test="inventory-item-name"]',{timeout:15000})
+    }
+    getInvetoryItemPrice() {
+        return cy.get('.inventory_item_price',{timeout:15000})
+    }
 }
 export default HomePage
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import LogInPage from "./Pages/loginPage";
+import HomePage from "./Pages/homePage";
+
+Cypress.Commands.add("login", function (url, username, password) {
+    const loginPage = new LogInPage()
+    const homePage = new HomePage()
+    cy.visit(url)
+    loginPage.getUsernameInput().should('be.visible').type(username);
+    loginPage.getPasswordInput().should('be.visible').type(password);
+    loginPage.getLogInButton().should('be.visible').click();
+    homePage.getHeader().should('be.visible').and('exist').invoke('text').then((titleText) => {
+        expect(titleText.trim()).to.include('Swag Labs')
+    })
+    homePage.getTitle().should('be.visible').and('exist').invoke('text').then((titleText) => {
+        expect(titleText.trim()).to.include('Products')
+    })
+    homePage.getShoppingCartIcon().should('exist').and('be.visible')
+    homePage.getProductsortContainer().should('exist').and('be.visible')
+})
+Cypress.Commands.add("numericalBubbleSort",function(arr){
+    let n = arr.length;
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] < arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    n--; 
+  } while (swapped);
+  return arr;
+})
