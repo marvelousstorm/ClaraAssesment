9923349f4583d92446d3d4c172f7841223b2afb2
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-05-29 13:11:27 -0600

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,11 +6,9 @@ Cypress.Commands.add("login", function (url, username, password) {
     const loginPage = new LogInPage()
     const homePage = new HomePage()
     cy.visit(url)
-    // cy.intercept('','/api/Login').as('loginAPI')
     loginPage.getUsernameInput().should('be.visible').type(username);
     loginPage.getPasswordInput().should('be.visible').type(password);
     loginPage.getLogInButton().should('be.visible').click();
-    // cy.wait('@loginAPI').its('response.statusCode').should('eq',200)
     homePage.getHeader().should('be.visible').and('exist').invoke('text').then((titleText) => {
         expect(titleText.trim()).to.include('Swag Labs')
     })
